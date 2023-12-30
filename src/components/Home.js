8a7a274fef1eca7ba551984aa1721f3fd7d800bd
Nidhish Srivastava <nidhish0430@gmail.com>
2023-12-30 12:46:38 +0530

    Fetching adminCollection documents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,52 @@
 "use client"
-import BottomNavBar from "./Navbar/BottomNavbar"
-import HomePageHeader from "./Navbar/HomePageHeader"
-import { collection, getDocs} from "firebase/firestore"
-import { db } from "@/utils/firebase"
-import { useEffect, useState } from "react"
-import ProjectCard from "./ProjectCard"
-import { sumHandler } from "@/utils"
+import BottomNavBar from "./Navbar/BottomNavbar";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import Hamburger from "./Navbar/Hamburger";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import InvestorCard from "./InvestorCard";
+
 
 function Home() {
-  const projectCollectionRef = collection(db, "projects")
-  const [popularProjects,setPopularProjects] = useState([])
-  useEffect(()=>{
-  const fetchPopularProjects = async() =>{
-    try {
-      const doc = await getDocs(projectCollectionRef)
-      const documentSumArray = [];
-       doc.forEach((doc)=>{
-        const data = doc.data()
-        const sum = sumHandler(data?.investmentProgress)
-        documentSumArray.push({ docId: doc.id, sum , ...data });
-      })
-      const sortedArray = documentSumArray.sort((a, b) => b.sum - a.sum);
-      setPopularProjects(sortedArray)
-      // Log the sorted array
+  const adminCollectionRef = collection(db, "admin")
+  const [investmentInterest, setInvestmentInterests] = useState([])
+  const [loading,setLoading] = useState(false)
 
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    const fetchInvestmentInterest = async () => {
+      setLoading(true)
+      try {
+        const data = await getDocs(adminCollectionRef)
+        setInvestmentInterests(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.log(error);
+      }
     }
-  }
-  fetchPopularProjects()
-  },[])
-
+    fetchInvestmentInterest()
+  }, [])
   return (
     <>
-    <HomePageHeader/>
-    <br />
-    <span className="ml-4 rounded-md  bg-blue-200 bg-opacity-19 text-blue-800 text-sm font-bold px-4 py-2">Popular Projects</span>
-    {/* <main className="mb-[5rem]">
-    {popularProjects.map((e,i)=>(
-      <ProjectCard key={i} projectObj={e}/>
+     <Hamburger/>
+      <header className="flex items-center gap-2 p-4 relative -z-[1] rounded-b-lg bg-blue-700 shadow-md">
+        <h2 className="font-semibold text-white">
+          Admin Panel
+        </h2>
+      </header>
+      <BottomNavBar />
+      {loading ?  <div style={{width : "80%",margin :"4rem auto"}}>
+            <Skeleton count={5}/>
+            </div> : 
+      <div className="py-4">
+      <span className="ml-4 rounded-md bg-blue-200 bg-opacity-19 text-blue-800 text-sm font-bold px-4 py-1">Investment Interests</span>
+      {investmentInterest?.map(e=>(
+        <InvestorCard key={e?.id} investorObj={e}/>
       ))}
-      </main> */}
-    <BottomNavBar/>
+    </div>
+      }
     </>
   )
 }
