a62f4716a2c6cf681cde360afb113e3d0159e136
Nidhish Srivastava <nidhish0430@gmail.com>
2024-06-15 01:06:19 +0530

    lint fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,10 @@
-export const sumHandler = (arr) =>{
-    const sum = arr?.reduce((acc,curr)=>{
-       return acc + curr.amountInvested
-      },0)
-      return sum
+export const sumHandler = (arr) => {
+  const sum = arr?.reduce((acc, curr) => {
+    return acc + curr.amountInvested
+  }, 0)
+  return sum
 }
 
 export const classNames = (...classes) => {
-    return classes.filter(Boolean).join(" ");
-  };
-  
+  return classes.filter(Boolean).join(" ");
+};
